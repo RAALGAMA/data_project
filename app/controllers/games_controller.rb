15701b58b6f2ed12c1b6a8727577597bae06a81a
Rafael Garcia <rafaelalejandrog7@gmail.com>
2023-10-18 10:26:18 -0500

    html5 part5

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.includes(:platform, :publisher).all.limit(1000)
+    @games = Game.includes(:platform, :publisher).all.limit(10)
 
     puts @sales.inspect
   end
